db892bc379f2b00f949edab490edd0ce70cefa66
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-08-31 18:02:35 +0000

    edit fields for the learning hours data class

--- app/src/main/java/me/tokornoe/gads_leaderboardapp/LearningHoursDataModel.kt
+++ app/src/main/java/me/tokornoe/gads_leaderboardapp/LearningHoursDataModel.kt
@@ -1,4 +1,4 @@
 package me.tokornoe.gads_leaderboardapp
 
 data class LearningHoursDataModel(val name: String, val hours: Int,
-                                  val country: String, val ImageUrl: String)
+                                  val country: String, val badgeUrl: String)
