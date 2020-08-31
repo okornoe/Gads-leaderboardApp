adf2c4604607dc4dfd0f6585f572267e72492dff
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-08-31 11:14:35 +0000

    implementation of the leader board by hours api service

--- app/src/main/java/me/tokornoe/gads_leaderboardapp/LeaderBoadApiService.kt
+++ app/src/main/java/me/tokornoe/gads_leaderboardapp/LeaderBoadApiService.kt
@@ -1,3 +1,33 @@
 package me.tokornoe.gads_leaderboardapp
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.Deferred
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
 private const val BASE_URL = " https://gadsapi.herokuapp.com"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .addCallAdapterFactory(CoroutineCallAdapterFactory())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface LeaderBoardApiService {
+    @GET("/api/hours")
+    fun getProperties():
+            Deferred<LearningHoursDataModel>
+}
+
+object LeaderBoardApi {
+    val retrofitService: LeaderBoardApiService by lazy {
+        retrofit.create(LeaderBoardApiService::class.java)
+    }
+}
