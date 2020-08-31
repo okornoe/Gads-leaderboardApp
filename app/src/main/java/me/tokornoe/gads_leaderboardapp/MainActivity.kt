9ce5f9296a5a0199e73a1a77483cb3139eb0d968
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-08-31 18:04:30 +0000

    bind view model to text view

--- app/src/main/java/me/tokornoe/gads_leaderboardapp/MainActivity.kt
+++ app/src/main/java/me/tokornoe/gads_leaderboardapp/MainActivity.kt
@@ -2,10 +2,23 @@ package me.tokornoe.gads_leaderboardapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import java.lang.Character.UnicodeBlock.of
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: LearningHoursLeaderBoardViewModel by lazy {
+        ViewModelProvider(this, LearningHoursLeaderBoardViewModel.Factory()).get(LearningHoursLeaderBoardViewModel::class.java)
+    }
+
+    private lateinit var learningHoursDataTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        learningHoursDataTextView = findViewById(R.id.tv_learning_hours_data)
+        learningHoursDataTextView.text = viewModel.response.value
     }
 }
