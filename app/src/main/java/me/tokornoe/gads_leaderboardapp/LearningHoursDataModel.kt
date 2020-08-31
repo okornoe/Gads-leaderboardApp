4038cc6103a96d8be0f6c0a25c75543391738fce
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-08-31 15:47:52 +0000

    add fields for the learning hours data class

--- app/src/main/java/me/tokornoe/gads_leaderboardapp/LearningHoursDataModel.kt
+++ app/src/main/java/me/tokornoe/gads_leaderboardapp/LearningHoursDataModel.kt
@@ -1,3 +1,4 @@
 package me.tokornoe.gads_leaderboardapp
 
-data class LearningHoursDataModel(val name: String, val hours: Int, val country: String, val ImageUrl: String)
+data class LearningHoursDataModel(val name: String, val hours: Int,
+                                  val country: String, val ImageUrl: String)
