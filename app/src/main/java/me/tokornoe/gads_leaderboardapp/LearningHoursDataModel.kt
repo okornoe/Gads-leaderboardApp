3c59a1c1b84a62b92e379955843a8b055940387b
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-09-01 22:09:06 +0000

    add LearningIQDataModel data class

--- app/src/main/java/me/tokornoe/gads_leaderboardapp/LearningHoursDataModel.kt
+++ app/src/main/java/me/tokornoe/gads_leaderboardapp/LearningHoursDataModel.kt
@@ -2,3 +2,5 @@ package me.tokornoe.gads_leaderboardapp
 
 data class LearningHoursDataModel(val name: String, val hours: Int,
                                   val country: String, val badgeUrl: String)
+
+data class LearningIQDataModel(val name: String, val score: Int, val country: String, val badgeUrl: String)
