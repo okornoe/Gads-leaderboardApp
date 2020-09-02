46f3e24bb318a4fa39b563ed003e5ef246b1c95b
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-09-02 16:27:47 +0000

    (refactor) move api service to network package

--- app/src/main/java/me/tokornoe/gads_leaderboardapp/network/LeaderBoadApiService.kt
+++ app/src/main/java/me/tokornoe/gads_leaderboardapp/network/LeaderBoadApiService.kt
@@ -1,9 +1,11 @@
-package me.tokornoe.gads_leaderboardapp
+package me.tokornoe.gads_leaderboardapp.network
 
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.Deferred
+import me.tokornoe.gads_leaderboardapp.model.LearningHoursDataModel
+import me.tokornoe.gads_leaderboardapp.model.LearningIQDataModel
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
