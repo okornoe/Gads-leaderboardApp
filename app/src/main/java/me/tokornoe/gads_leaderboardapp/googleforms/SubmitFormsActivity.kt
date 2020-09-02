55a0f6d544af2630b31f7b5db599ef69efb34a7d
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-09-02 13:41:24 +0000

    add submit forms activity

---
+++ app/src/main/java/me/tokornoe/gads_leaderboardapp/googleforms/SubmitFormsActivity.kt
@@ -0,0 +1,12 @@
+package me.tokornoe.gads_leaderboardapp.googleforms
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.tokornoe.gads_leaderboardapp.R
+
+class SubmitFormsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_submit_forms)
+    }
+}
