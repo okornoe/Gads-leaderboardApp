d0ef18d8f13fc9007e0f1023a91eed8311a7dea5
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-09-03 15:30:32 +0000

    remove un needed code

--- app/src/main/java/me/tokornoe/gads_leaderboardapp/MainActivity.kt
+++ app/src/main/java/me/tokornoe/gads_leaderboardapp/MainActivity.kt
@@ -2,39 +2,14 @@ package me.tokornoe.gads_leaderboardapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import me.tokornoe.gads_leaderboardapp.leaderboard_skill_iq.LearningIQLeaderBoardViewModel
-
+import androidx.databinding.DataBindingUtil
+import me.tokornoe.gads_leaderboardapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-/*
-    private val viewModel: LearningHoursLeaderBoardViewModel by lazy {
-        ViewModelProvider(this, LearningHoursLeaderBoardViewModel.Factory()).get(LearningHoursLeaderBoardViewModel::class.java)
-    }*/
-
-    //private lateinit var hoursViewModel: LearningHoursLeaderBoardViewModel
-    private lateinit var iQViewModel: LearningIQLeaderBoardViewModel
-
-    private lateinit var learningHoursDataTextView: TextView
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-/*        hoursViewModel = ViewModelProvider(this).get(LearningHoursLeaderBoardViewModel::class.java)
-        hoursViewModel.hoursDataList.observe(this, Observer { data ->
-            data.listIterator().hasNext()
-        })*/
-
-        iQViewModel = ViewModelProvider(this).get(LearningIQLeaderBoardViewModel::class.java)
-        iQViewModel.iQList.observe(this, Observer { data ->
-            data.listIterator().hasNext()
-        })
-
-        learningHoursDataTextView = findViewById(R.id.tv_learning_hours_data)
-
-        learningHoursDataTextView.text = iQViewModel.iQList.value.toString()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
