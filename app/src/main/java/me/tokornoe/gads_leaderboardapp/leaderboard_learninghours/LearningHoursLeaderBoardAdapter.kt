5d1fff758fcf9af274496b7c1ca4b7c86c190bbd
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-09-03 15:33:30 +0000

    add parcelize annotation

--- app/src/main/java/me/tokornoe/gads_leaderboardapp/leaderboard_learninghours/LearningHoursLeaderBoardAdapter.kt
+++ app/src/main/java/me/tokornoe/gads_leaderboardapp/leaderboard_learninghours/LearningHoursLeaderBoardAdapter.kt
@@ -1,5 +1,14 @@
 package me.tokornoe.gads_leaderboardapp.leaderboard_learninghours
 
 import android.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import me.tokornoe.gads_leaderboardapp.databinding.ListItemBinding
+import me.tokornoe.gads_leaderboardapp.model.LearningHoursDataModel
 
-//class LearningHoursLeaderBoardAdapter : ListAdapter<LearningHoursDataModel>
+/*class LearningHoursLeaderBoardAdapter : ListAdapter<LearningHoursDataModel, LearningHoursLeaderBoardAdapter.ViewHolder>() {
+    5
+    class ViewHolder (private val binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root)
+}*/
+
+
+
--- app/src/main/java/me/tokornoe/gads_leaderboardapp/model/LearningHoursDataModel.kt
+++ app/src/main/java/me/tokornoe/gads_leaderboardapp/model/LearningHoursDataModel.kt
@@ -1,6 +1,12 @@
 package me.tokornoe.gads_leaderboardapp.model
 
-data class LearningHoursDataModel(val name: String, val hours: Int,
-                                  val country: String, val badgeUrl: String)
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
-data class LearningIQDataModel(val name: String, val score: Int, val country: String, val badgeUrl: String)
+@Parcelize
+data class LearningHoursDataModel(val name: String, val hours: Int,
+                                  val country: String, val badgeUrl: String) : Parcelable
+
+@Parcelize
+data class LearningIQDataModel(val name: String, val score: Int,
+                               val country: String, val badgeUrl: String) : Parcelable
