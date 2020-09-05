a50da6976a8251dc4aadf2819aaa4c09f584df7b
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-09-05 16:53:09 +0000

    implementing LearningHoursLeaderBoardAdapter buggy

--- app/src/main/java/me/tokornoe/gads_leaderboardapp/leaderboard_learninghours/LearningHoursLeaderBoardAdapter.kt
+++ app/src/main/java/me/tokornoe/gads_leaderboardapp/leaderboard_learninghours/LearningHoursLeaderBoardAdapter.kt
@@ -1,14 +1,51 @@
 package me.tokornoe.gads_leaderboardapp.leaderboard_learninghours
 
-import android.widget.ListAdapter
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import me.tokornoe.gads_leaderboardapp.R
 import me.tokornoe.gads_leaderboardapp.databinding.ListItemBinding
 import me.tokornoe.gads_leaderboardapp.model.LearningHoursDataModel
 
-/*class LearningHoursLeaderBoardAdapter : ListAdapter<LearningHoursDataModel, LearningHoursLeaderBoardAdapter.ViewHolder>() {
-    5
-    class ViewHolder (private val binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root)
-}*/
+class LearningHoursLeaderBoardAdapter :
+    ListAdapter<LearningHoursDataModel, LearningHoursLeaderBoardAdapter.LearningHoursViewHolder>(LearningHoursDiffCallback()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LearningHoursViewHolder {
+        return LearningHoursViewHolder(
+            DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context),
+                R.layout.list_item, parent, false
+            )
+        )
+    }
 
+    override fun onBindViewHolder(holder: LearningHoursViewHolder, position: Int) {
+        val learningHoursData = getItem(position)
+        holder.bind(learningHoursData)
+    }
 
+    class LearningHoursViewHolder(private val binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(learningHoursDataModel: LearningHoursDataModel) {
+            binding.property = learningHoursDataModel
+            binding.executePendingBindings()
+        }
+    }
+}
 
+private class LearningHoursDiffCallback : DiffUtil.ItemCallback<LearningHoursDataModel>() {
+    override fun areItemsTheSame(
+        oldItem: LearningHoursDataModel,
+        newItem: LearningHoursDataModel
+    ): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(
+        oldItem: LearningHoursDataModel,
+        newItem: LearningHoursDataModel
+    ): Boolean {
+        return oldItem == newItem
+    }
+}
