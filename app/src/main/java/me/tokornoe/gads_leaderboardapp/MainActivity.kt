e81a3635d4aee0e4900bd2e3dd16c9e8bf97653a
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-09-08 13:10:24 +0000

    initialize view pager and tab layout

--- app/src/main/java/me/tokornoe/gads_leaderboardapp/MainActivity.kt
+++ app/src/main/java/me/tokornoe/gads_leaderboardapp/MainActivity.kt
@@ -2,13 +2,23 @@ package me.tokornoe.gads_leaderboardapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
-import me.tokornoe.gads_leaderboardapp.databinding.ActivityMainBinding
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var tabLayout: TabLayout
+    lateinit var viewPager: ViewPager2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView( R.layout.activity_main)
+
+
+        tabLayout =findViewById(R.id.tab_layout)
+        viewPager = findViewById(R.id.pager)
+
+        tabLayout.addTab(tabLayout.newTab().setText("Learning Hours Leader board"))
+        tabLayout.addTab(tabLayout.newTab().setText("IQ leader board"))
     }
 }
